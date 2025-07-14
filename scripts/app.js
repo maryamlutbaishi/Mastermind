494576a598b62d11b4de86e3729110858f5a2df7
maryam <maryamlutbaishi213@hotmail.com>
2025-07-14 21:32:20 +0300

    make the boxes for inputs and add event listener for the keybord

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,70 @@
 function init(){
 /*-------------------------------- Constants --------------------------------*/
-
+const matriLength=4
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let guess=['','','','']
+let randCode=[]
+let turn
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const allBoxEl=document.querySelectorAll('.boxes')
+const allInput=document.querySelectorAll('input')
 
 /*-------------------------------- Functions --------------------------------*/
+function moveToNextInput(currentInput){
+    const idx=Array.from(allInput).indexOf(currentInput)
+    const nextIdx=idx+1
+    if(nextIdx<allInput.length){
+      allInput[nextIdx].focus()
+    }
+  }
 
+function handelkey(k){
+    const key=k.key
+
+    const allowedKeys=['0','1','2','3','4','5','6','7','8','9','backspace','enter']
+    if(!allowedKeys.includes(key)){
+        k.preventDefault()
+
+
+    }
+    if (key === 'Enter') {
+      k.preventDefault();        
+      moveToNextInput(k.target);                
+      return;                      
+    }
+
+    
+    if (key === 'Backspace'){ return;
+
+    }
+
+}
+
+
+function handleInput(num){
+const input=num.target
+const value = input.value
+guess.push(value)
+console.log(guess)
+// const index=Array.from(allInput).indexOf(input)
+// value=value.slice(0,1)
+
+
+}
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+allBoxEl.forEach(box=>{
+    box.addEventListener('input', handleInput)
+    box.addEventListener('keydown',handelkey)
+})
+
+
 }
+
 document.addEventListener('DOMContentLoaded',init)
